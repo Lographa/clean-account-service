d56f8d28dd1d4593ef897f8ab402c31e680c433e
Raphael <81043894+Lographa@users.noreply.github.com>
2026-09-15 13:59:15 -0300

    first commit

---
+++ src/test/java/com/raphael/account_transfer/AccountTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.account_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
